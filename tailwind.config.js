62205dcdc0a162a5757c057dfd9400965e4cc0ec
Imran <imranjjj3@gmail.com>
2021-08-07 12:03:37 +0600

    Commit changes

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        xxl: "7px",
+      },
+      spacing: {
+        m75: "75px",
+        m84: "84px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+
+  plugins: [],
+};
